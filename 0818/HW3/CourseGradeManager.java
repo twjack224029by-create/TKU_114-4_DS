b2ba969725e77a72f08fc4c56cad3e615bb6569f
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-20 03:59:30 +0800

    Update CourseGradeManager.java

--- 0818/HW3/CourseGradeManager.java
+++ 0818/HW3/CourseGradeManager.java
@@ -54,3 +54,50 @@ class CourseGrade {
                 studentId, name, regularScore, midtermScore, finalScore, attendanceScore, calculateFinalScore(), getLevel());
     }
 }
+
+public class CourseGradeManager {
+    public static void main(String[] args) {
+        CourseGrade[] students = new CourseGrade[] {
+            new CourseGrade("01", "迪迪", 85.0, 78.0, 92.0, 100.0),
+            new CourseGrade("02", "糙匹", 95.0, 88.0, 90.0, 90.0),
+            new CourseGrade("03", "阿強", 40.0, 50.0, 45.0, 60.0), 
+            new CourseGrade("04", "阿傑", 70.0, 65.0, 80.0, 85.0),
+            new CourseGrade("05", "白白", 30.0, 20.0, 40.0, 50.0)  
+        };
+
+        System.out.println("成績明細:");
+        for (CourseGrade student : students) {
+            System.out.println(student.toString());
+        }
+
+        double totalSum = 0;
+        for (CourseGrade student : students) {
+            totalSum += student.calculateFinalScore();
+        }
+        double classAverage = totalSum / students.length;
+        System.out.printf("全班平均總分: %.2f 分%n", classAverage);
+
+        CourseGrade topStudent = students[0];
+        for (int i = 1; i < students.length; i++) {
+            if (students[i].calculateFinalScore() > topStudent.calculateFinalScore()) {
+                topStudent = students[i];
+            }
+        }
+        System.out.printf("全班最高分學生: %s (%s)，總分: %.2f 分 (等級 %s)%n",
+                topStudent.getName(), topStudent.getStudentId(), topStudent.calculateFinalScore(), topStudent.getLevel());
+
+        System.out.println("\n 不及格");
+        boolean hasFailedStudents = false;
+        for (CourseGrade student : students) {
+            if (student.calculateFinalScore() < 60.0) {
+                System.out.printf("學號: %s  姓名: %s  總分: %.2f (等級: %s)%n",
+                        student.getStudentId(), student.getName(), student.calculateFinalScore(), student.getLevel());
+                hasFailedStudents = true;
+            }
+        }
+
+        if (!hasFailedStudents) {
+            System.out.println("無不及格學生。");
+        }
+    }
+}
