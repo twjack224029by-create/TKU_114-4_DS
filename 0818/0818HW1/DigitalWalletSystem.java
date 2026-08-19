c89fe00de4018a8ecae9786b1077a53ec8f406c5
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-19 17:38:47 +0800

    Update DigitalWalletSystem.java

--- 0818/0818HW1/DigitalWalletSystem.java
+++ 0818/0818HW1/DigitalWalletSystem.java
@@ -6,9 +6,9 @@ class DigitalWallet{
   private double balance;
   private int totalTransactions;
 
-  public DigitalWallet(String Walletid,String owner,double initialbalance){
-    if(Walletid == null || Walletid.trim().isEmpty())
-    if()
-    if()
+  public DigitalWallet(String Walletid,String owner,double initialBalance){
+    if(Walletid == null || Walletid.trim().isEmpty()){throw new IllegalArgumentException("請填寫id");}
+    if(owner == null || owner.trim().isEmpty()){throw new IllegalArgumentException("請輸入姓名");)
+    if(initialbalance<0){throw new IllegalArgumentException("初始餘額不可為負");}
   }
 }
