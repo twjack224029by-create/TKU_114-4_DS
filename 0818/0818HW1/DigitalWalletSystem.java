77afbbca1db47a486619708dc64cfbee53fdb4e5
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-20 01:40:47 +0800

    Update DigitalWalletSystem.java

--- 0818/0818HW1/DigitalWalletSystem.java
+++ 0818/0818HW1/DigitalWalletSystem.java
@@ -17,6 +17,12 @@ class DigitalWallet{
   this.balance=initialBalance;
   this.totalTransactions=0;
 }
+
+public String getWalletid(){return Walletid;}
+public String getowner(){return owner;}
+public Double getBalance(){return balance;}
+public int getTotalTransactions(){return totalTransactions;}
+
 /*加錢*/
 public boolean deposit(double amount){
   if(amount<=0){
@@ -28,6 +34,7 @@ public boolean deposit(double amount){
   System.out.printf("成功加值 $%.2f, 當前餘額: $%2.f%n",amount,this.balance);
   return true;
 }
+
 /*付款*/
 public boolean pay(double amount){
   if(amount<=0){
@@ -43,6 +50,7 @@ public boolean pay(double amount){
   System.out.printf("成功付款 金額: $%.2f, 當前餘額: $%2.f%n",amount,this.balance);
   return true;
 }
+
 /*退錢*/
 public boolean refund(double amount){
   if(amount<=0){
@@ -54,6 +62,7 @@ public boolean refund(double amount){
   System.out.printf("成功退款 金額: $%.2f, 當前餘額: $%2.f%n",amount,this.balance);
   return true;
 }
+
 /*帳號資料*/
 public void printStatus(){
   System.out.printf("錢包id: %s 持有人: %s%n",Walletid,owner);
