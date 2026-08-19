5a588dd140d2c5020bb490c99b9eeadffc7a4a37
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-20 02:51:08 +0800

    Update CustomerOrderSystem.java

--- 0818/HW2/CustomerOrderSystem.java
+++ 0818/HW2/CustomerOrderSystem.java
@@ -3,4 +3,13 @@ class customer{
   private final String customerId;
   private final String name;
   private final String email;
+
+  public Customer(String customerId,String name, String email){
+    this.customerId = customerId;
+    this.name = name;
+    this.email = email;
+  }
+  public String getCustomerId(){return customerId;}
+  public String getName(){return name;}
+  public String getEmail(){return email;}
 }
