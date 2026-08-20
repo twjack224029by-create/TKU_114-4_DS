77198be9e302ec611e20b0449846774ea13f63df
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-20 18:03:34 +0800

    Update PayrollPolymorphismSystem.java

--- 0819/hwA/PayrollPolymorphismSystem.java
+++ 0819/hwA/PayrollPolymorphismSystem.java
@@ -55,3 +55,41 @@ class Taxi extends Transport {
     }
 }
 
+public class TransportFareSystem {
+    public static void main(String[] args) {
+        System.out.println("test \n");
+
+        Transport[] transports = new Transport[] {
+            new Bus("紅 23 號公車"),
+            new Bus("307 路線公車"),
+            new Taxi("大都會計程車 (T-001)"),
+            new Taxi("台灣大車隊 (T-002)")
+        };
+
+        int testDistance = 15; 
+
+        System.out.printf("模擬所有交通工具里程: %d 公里%n", testDistance);
+
+        for (int i = 0; i < transports.length; i++) {
+            Transport transport = transports[i];
+            int fare = transport.calculateFare(testDistance); 
+
+            System.out.printf("%d. [%s] -> 路線/車號: %-18s | 計算票價: $%d 元%n",
+                    (i + 1), 
+                    transport.getClass().getSimpleName(), 
+                    transport.getRouteName(), 
+                    fare);
+        }
+
+
+        System.out.println("\n不同里程測試");
+        int[] distances = { 1, 5, 10, 25 };
+
+        for (int dist : distances) {
+            System.out.printf("\n--- 當里程為 %d 公里時 ---%n", dist);
+            for (Transport t : transports) {
+                System.out.printf("  %-20s 票價: $%d%n", t.getRouteName(), t.calculateFare(dist));
+            }
+        }
+    }
+}
