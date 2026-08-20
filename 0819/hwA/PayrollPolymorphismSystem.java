b0a9c7b6a6996c05b2f0ca9c5d15708b4c20bd6c
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-20 17:47:41 +0800

    Update PayrollPolymorphismSystem.java

--- 0819/hwA/PayrollPolymorphismSystem.java
+++ 0819/hwA/PayrollPolymorphismSystem.java
@@ -15,3 +15,20 @@ abstract class Transport {
     }
     public abstract int calculateFare(int distance);
 }
+
+class Bus extends Transport {
+    private static final int BASE_FARE = 15;
+    private static final int SECTION_DISTANCE = 8;
+
+    public Bus(String routeName) {
+        super(routeName);
+    }
+
+    @Override
+    public int calculateFare(int distance) {
+        if (distance <= 0) return 0;
+        int sections = (int) Math.ceil((double) distance / SECTION_DISTANCE);
+        return sections * BASE_FARE;
+    }
+}
+
