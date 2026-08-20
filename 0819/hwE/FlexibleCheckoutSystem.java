0bfa36a5764508426898e76fa244f8cb16579b8f
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-20 16:55:57 +0800

    Update FlexibleCheckoutSystem.java

--- 0819/hwE/FlexibleCheckoutSystem.java
+++ 0819/hwE/FlexibleCheckoutSystem.java
@@ -1,1 +1,31 @@
+import java.util.Objects;
+interface PricingPolicy {
+    int finalPrice(int originalPrice);
+}
+
+class StandardPricing implements PricingPolicy {
+    @Override
+    public int finalPrice(int originalPrice) {
+        return Math.max(0, originalPrice);
+    }
+}
+
+class VipPricing implements PricingPolicy {
+    @Override
+    public int finalPrice(int originalPrice) {
+        return Math.max(0, originalPrice) * 85 / 100;
+    }
+}
+
+class ThresholdDiscountPricing implements PricingPolicy {
+    @Override
+    public int finalPrice(int originalPrice) {
+        int validPrice = Math.max(0, originalPrice);
+        if (validPrice >= 2000) {
+            return validPrice - 300;
+        }
+        return validPrice;
+    }
+}
+
 
