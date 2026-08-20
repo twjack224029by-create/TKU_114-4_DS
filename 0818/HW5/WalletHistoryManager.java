6e839b09eeaa438d17c11ba739c06bb978ffa547
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-20 15:21:41 +0800

    Update WalletHistoryManager.java

--- 0818/HW5/WalletHistoryManager.java
+++ 0818/HW5/WalletHistoryManager.java
@@ -1,1 +1,20 @@
+import java.util.Objects;
+class Transaction {
+  private final int sequence;
+  private final String type;
+  private final double amount;
 
+  public Transaction(int sequence, String type, double amount) {
+        this.sequence = sequence;
+        this.type = type;
+        this.amount = amount;
+    }
+  public int getSequence() {return sequence;}
+  public String getType() {return type;}
+  public double getAmount() {return amount;}
+
+    @Override
+    public String toString() {
+        return String.format("  [序號 #%02d] 類型: %-12s | 金額: $%8.2f", sequence, type, amount);
+    }
+}
