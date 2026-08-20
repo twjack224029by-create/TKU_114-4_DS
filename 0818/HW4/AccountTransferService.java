dcdce4d3783479c80bed0d1735866f1b04b1460a
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-20 14:06:29 +0800

    Update AccountTransferService.java

--- 0818/HW4/AccountTransferService.java
+++ 0818/HW4/AccountTransferService.java
@@ -5,16 +5,16 @@ class Account{
   private int balance;
 
   public Account(String accountNumber, String ownerName, int initialBalance){
-    if(){}
-    if(){}
-    if(){}
-    this. =;
-    this. =;
-    this. =;
+    if(accountNumber == null || accountNumber.trim().isEmpty()){throw new IllegalArgumentException("帳號名稱不得為空");}
+    if(ownerName == null || ownerName.trim().isEmpty()){ throw new IllegalArgumentException("戶名不得為空");}
+    if(initialBalance < 0){throw new IllegalArgumentException("初始餘額不得為負數");}
+    this.accountNumber = accountNumber;
+    this.ownerName = ownerName;
+    this.balance = balance;
   }
-  public String get(){return;}
-  public String get(){return;}
-  public int get(){return;}
+  public String get(AccountNumber){return accountNumber;}
+  public String get(OwnerName){return ownerName;}
+  public int get(Balance){return balance;}
 
   void withdraw(int amount) {
         this.balance -= amount;
@@ -22,4 +22,12 @@ class Account{
   void deposit(int amount) {
         this.balance += amount;
     }
+  @Override
+    public String toString() {
+        return String.format("帳號: %s  戶名: %s  餘額: $%d", accountNumber, ownerName, balance);
+    }
 }
+
+
+
+
