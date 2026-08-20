06441164cdf7021d30d3232103552091567780e2
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-20 21:54:15 +0800

    Update MediaProcessingSystem.java

--- 0819/hwC/MediaProcessingSystem.java
+++ 0819/hwC/MediaProcessingSystem.java
@@ -6,3 +6,30 @@ interface Exportable {
 interface Compressible {
     void compress(String algorithm);
 }
+
+class BackupDocument implements Exportable, Compressible {
+    String docName;
+
+    
+    public BackupDocument(String docName) {
+        this.docName = docName;
+    }
+
+
+    @Override
+    public void exportToFile(String path) {
+        System.out.println("成功把檔案 " + docName + " 匯出到: " + path);
+    }
+
+    
+    @Override
+    public void compress(String algorithm) {
+        System.out.println("成功使用 " + algorithm + " 演算法壓縮檔案: " + docName);
+    }
+
+    
+    public void printName() {
+        System.out.println("檔案名稱是: " + docName);
+    }
+}
+
