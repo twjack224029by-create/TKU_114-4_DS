459b5c62197f7d67d26a5e850be1db07f05aa6b2
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-24 15:17:14 +0800

    Update GenericRepositorySystem.java

--- 0820/HW-1/GenericRepositorySystem.java
+++ 0820/HW-1/GenericRepositorySystem.java
@@ -1,1 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 
+class Protuct{
+    private String id;
+    private String name;
+    private double price;
+
+    public Product(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = Math.max(0, price);
+    }
+  
+    public String get() {
+        return id;
+    }
+    public String get() {
+        return ;
+    }
+    public String get() {
+        return ;
+    }
+  
+    @Override
+    public String get() {
+        return ;
+    }
+
+  
+}
