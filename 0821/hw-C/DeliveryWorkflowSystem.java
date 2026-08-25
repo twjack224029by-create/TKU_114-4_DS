5cd073b3a7a987adb5e11fa7f811c7d2662e16f1
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-25 21:32:47 +0800

    Update DeliveryWorkflowSystem.java

--- 0821/hw-C/DeliveryWorkflowSystem.java
+++ 0821/hw-C/DeliveryWorkflowSystem.java
@@ -40,7 +40,44 @@ class DeliveryTask {
 }
 
 public class DeliveryWorkflowSystem {
-  
+    private final Map<String, DeliveryTask> taskMap = new HashMap<>();
+    private final Queue<DeliveryTask> waitingQueue = new ArrayDeque<>();
+    private final Deque<DeliveryTask> completedStack = new ArrayDeque<>();
+
+    public boolean addDelivery(String id, String address) {
+        if (id == null || id.isBlank()) {
+            System.out.println("失敗,配送編號不可為空");
+            return false;
+        }
+
+        if (taskMap.containsKey(id)) {
+            System.out.println("失敗:,配送編號 [" + id + "] 已存在，不可重複加入");
+            return false;
+        }
+
+        DeliveryTask task = new DeliveryTask(id, address);
+        taskMap.put(id, task);
+        waitingQueue.offer(task);
+        System.out.println("-> 成功新增配送包裹: " + task);
+        printStatus();
+        return true;
+    }
+
+    public DeliveryTask processNextDelivery() {
+        if (waitingQueue.isEmpty()) {
+            System.out.println("失敗,現在沒有等待配送的包裹");
+            return null;
+        }
+
+        DeliveryTask task = waitingQueue.poll();
+        task.setStatus("DELIVERED");
+        completedStack.push(task); 
+        System.out.println("成功處理配送包裹: " + task);
+        printStatus();
+        return task;
+    }
+
+    
 }
 
 
