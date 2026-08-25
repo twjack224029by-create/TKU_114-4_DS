b57f8edbfd2f0dd6db5f2ec4651031d32a333444
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-25 21:00:56 +0800

    Update ClinicQueueSystem.java

--- 0821/hw-B/ClinicQueueSystem.java
+++ 0821/hw-B/ClinicQueueSystem.java
@@ -1,1 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
+class Patient {
+    private final String id;   
+    private final String name; 
+
+    public Patient(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + name;
+    }
+}
+
+public class ClinicQueueSystem {
+  
+}
