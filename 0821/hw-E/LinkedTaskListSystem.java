0f39485f0ea7e36b4d0b850c981de2f5d59d146a
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-25 22:20:36 +0800

    Update LinkedTaskListSystem.java

--- 0821/hw-E/LinkedTaskListSystem.java
+++ 0821/hw-E/LinkedTaskListSystem.java
@@ -1,1 +1,57 @@
+class Task {
+    private final String id;
+    private final String description;
 
+    public Task(String id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + description;
+    }
+}
+
+class TaskNode {
+    Task task;
+    TaskNode next;
+
+    public TaskNode(Task task) {
+        this.task = task;
+        this.next = null;
+    }
+}
+
+class TaskLinkedList {
+    private TaskNode head;
+    private int size;
+
+    public TaskLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+      public int size() {
+        return size;
+    }
+
+  public boolean containsId(String id) {
+        if (id == null) return false;
+        TaskNode current = head;
+        while (current != null) {
+            if (current.task.getId().equalsIgnoreCase(id)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
