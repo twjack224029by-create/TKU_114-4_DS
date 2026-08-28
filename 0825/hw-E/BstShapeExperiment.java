239d5dce60cbfe008b43b066a37dfaf1384ae81f
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-28 14:44:08 +0800

    Update BstShapeExperiment.java

--- 0825/hw-E/BstShapeExperiment.java
+++ 0825/hw-E/BstShapeExperiment.java
@@ -1,1 +1,80 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class ExperimentBst {
+    private TreeNode root;
+
+    public ExperimentBst() {
+        this.root = null;
+    }
+
+    public void insert(int val) {
+        root = insertHelper(root, val);
+    }
+
+    private TreeNode insertHelper(TreeNode node, int val) {
+        if (node == null) {
+            return new TreeNode(val);
+        }
+        if (val < node.val) {
+            node.left = insertHelper(node.left, val);
+        } else if (val > node.val) {
+            node.right = insertHelper(node.right, val);
+        }
+        return node;
+    }
+
+    public int getHeight() {
+        return getHeightHelper(root);
+    }
+
+    private int getHeightHelper(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + Math.max(getHeightHelper(node.left), getHeightHelper(node.right));
+    }
+
+    public int getSearchComparisonCount(int target) {
+        return searchHelper(root, target, 0);
+    }
+
+    private int searchHelper(TreeNode node, int target, int currentComparisons) {
+        if (node == null) {
+            return currentComparisons; 
+        }
+
+        currentComparisons++;
+
+        if (target == node.val) {
+            return currentComparisons;
+        } else if (target < node.val) {
+            return searchHelper(node.left, target, currentComparisons);
+        } else {
+            return searchHelper(node.right, target, currentComparisons);
+        }
+    }
+
+    public int getTotalComparisonsForAllNodes(List<Integer> values) {
+        int total = 0;
+        for (int val : values) {
+            total += getSearchComparisonCount(val);
+        }
+        return total;
+    }
+}
 
