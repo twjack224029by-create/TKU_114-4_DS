25f41e3f79cc7eb07f297ea8b2c774e7d4b4686b
twjack224029by-create <tw.jack224029by@gmail.com>
2026-08-31 23:59:41 +0800

    Update TopSellingProducts.java

--- 0831/HW-D/TopSellingProducts.java
+++ 0831/HW-D/TopSellingProducts.java
@@ -77,5 +77,62 @@ public class TopSellingProducts {
     public void clear() {
         salesMap.clear();
     }
-  
+
+    public static void main(String[] args) {
+        TopSellingProducts tracker = new TopSellingProducts();
+
+        System.out.println("                TopSellingProducts");
+
+        System.out.println("模擬輸入包含重複 ID 的多筆交易數據");
+
+        Object[][] rawTransactions = {
+            {"A", 150},
+            {"B", 300},
+            {"C", 50},
+            {"A", 200}, 
+            {"D", 300}, 
+            {"E", 500}, 
+            {"F", 100},
+            {"B", 50},  
+            {"G", 350} 
+        };
+
+        for (Object[] tx : rawTransactions) {
+            String id = (String) tx[0];
+            int sales = (Integer) tx[1];
+            tracker.recordSale(id, sales);
+            System.out.printf("  [交易紀錄] 商品: %-8s +%-3d 件%n", id, sales);
+        }
+
+        System.out.println("銷量合併預期結果說明：");
+        System.out.println("  E: 500");
+        System.out.println("  A: 350 ");
+        System.out.println("  B: 350");
+        System.out.println("  G: 350");
+        System.out.println("  D: 300");
+
+        System.out.println("測試前3熱門商品");
+        List<Product> top3 = tracker.getTopK(3);
+        printReport(top3, 3);
+
+        System.out.println("測試前5熱門商品");
+        List<Product> top5 = tracker.getTopK(5);
+        printReport(top5, 5);
+
+        System.out.println("測試K>商品種類數");
+        List<Product> top10 = tracker.getTopK(10);
+        printReport(top10, 10);
+    }
+
+    private static void printReport(List<Product> products, int k) {
+        System.out.println("前" + k + " 排行榜結果");
+        if (products.isEmpty()) {
+            System.out.println("  (無資料)");
+        } else {
+            for (int i = 0; i < products.size(); i++) {
+                System.out.printf("  Rank %02d: %s%n", (i + 1), products.get(i));
+            }
+        }
+        System.out.println();
+    }
 }
