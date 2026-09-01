f1066057a70346ce3cb584c1e3f0f72d7db635fb
twjack224029by-create <tw.jack224029by@gmail.com>
2026-09-01 14:35:47 +0800

    Update BookIsbnHashTable.java

--- 0901/hw-1/BookIsbnHashTable.java
+++ 0901/hw-1/BookIsbnHashTable.java
@@ -1,1 +1,42 @@
+class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private int price;
 
+    public Book(String isbn, String title, String author, int price) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    // Getters and Setters
+    public String getIsbn() { return isbn; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public int getPrice() { return price; }
+    public void setTitle(String title) { this.title = title; }
+    public void setAuthor(String author) { this.author = author; }
+    public void setPrice(int price) { this.price = price; }
+
+    @Override
+    public String toString() {
+        return String.format("ISBN: %-13s  書名: %-18s  作者: %-10s  價格: NT$%d",
+                isbn, title, author, price);
+    }
+}
+
+class BookNode {
+    Book book;
+    BookNode next;
+
+    public BookNode(Book book) {
+        this.book = book;
+        this.next = null;
+    }
+}
+
+public class BookIsbnHashTable {
+  
+}
