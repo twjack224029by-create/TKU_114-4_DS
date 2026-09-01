bddadc3fabb0cd5b6884ce6efea7d806ed194b27
twjack224029by-create <tw.jack224029by@gmail.com>
2026-09-01 15:23:53 +0800

    Update LoginActivityReport.java

--- 0901/hw-2/LoginActivityReport.java
+++ 0901/hw-2/LoginActivityReport.java
@@ -1,1 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+class LoginLog {
+    private String username;
+    private String ipAddress;
+    private String timestamp;
+
+    public LoginLog(String username, String ipAddress, String timestamp) {
+        this.username = username;
+        this.ipAddress = ipAddress;
+        this.timestamp = timestamp;
+    }
+
+    public String getUsername() { return username; }
+    public String getIpAddress() { return ipAddress; }
+    public String getTimestamp() { return timestamp; }
+}
+
+class AccountStats {
+    private String username;
+    private int totalLogins;             
+    private Set<String> uniqueIps;       
+
+    public AccountStats(String username) {
+        this.username = username;
+        this.totalLogins = 0;
+        this.uniqueIps = new HashSet<>();
+    }
+
+    public void addLogin(String ip) {
+        this.totalLogins++;
+        this.uniqueIps.add(ip);
+    }
+
+    public String getUsername() { return username; }
+    public int getTotalLogins() { return totalLogins; }
+    public Set<String> getUniqueIps() { return uniqueIps; }
+    public int getUniqueIpCount() { return uniqueIps.size(); }
+}
