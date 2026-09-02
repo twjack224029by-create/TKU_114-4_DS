a161d647df1344da8b11cfc21e831909c12aef9a
twjack224029by-create <tw.jack224029by@gmail.com>
2026-09-02 16:30:19 +0800

    Update CampusNavigationSystem.java

--- 0902/HW-A/CampusNavigationSystem.java
+++ 0902/HW-A/CampusNavigationSystem.java
@@ -1,1 +1,40 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
+public class CampusNavigationSystem {
+  private final Map<String, String> locations;
+    private final Map<String, List<String>> roadNetwork;
+
+    public CampusNavigationSystem() {
+        this.locations = new HashMap<>();
+        this.roadNetwork = new LinkedHashMap<>();
+    }
+
+    public void addLocation(String id, String name) {
+        if (id == null || name == null) return;
+        locations.put(id, name);
+        roadNetwork.putIfAbsent(id, new ArrayList<>());
+    }
+
+    public void addRoad(String loc1, String loc2) {
+        if (!locations.containsKey(loc1) || !locations.containsKey(loc2)) {
+            System.out.printf("失敗,指定地點不存在: %s 或 %s%n", loc1, loc2);
+            return;
+        }
+
+        List<String> neighbors1 = roadNetwork.get(loc1);
+        if (!neighbors1.contains(loc2)) neighbors1.add(loc2);
+
+        List<String> neighbors2 = roadNetwork.get(loc2);
+        if (!neighbors2.contains(loc1)) neighbors2.add(loc1);
+    }
+    
+}
